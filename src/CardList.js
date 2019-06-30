10e50de7f8c0a63d428719c1fdb894f29841cf35
pranav2308 <pranav08@terpmail.umd.edu>
2019-06-29 21:07:01 -0400

    Refactoring code

---
+++ src/CardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card'
+
+const returnCardElement = (user) => {
+	let element = <Card key = {user.id} id = {user.id} name = {user.name} email = {user.email}/>
+	return element;
+}
+
+const CardList = ({ robots }) => {
+	const produceCardList = robots.map(returnCardElement);
+	return (
+		<div>
+			{produceCardList};
+		</div>
+	);
+}
+
+
+
+export default CardList;
+
