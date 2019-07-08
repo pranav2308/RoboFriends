3160b5e80e4a714dc9573887cf943822bbcdb087
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-07 21:16:58 -0400

    Adding search function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import CardList from './CardList';
 import { robots } from './robots'
 import SearchBox from './SearchBox';
 
-const App = () => {
-	return(
-		<div className = 'tc'>
-			<h1> RoboFriends </h1>
-			<SearchBox />
-			<CardList robots = {robots} />
-		</div>
-	);
-}
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			robots : robots,
+			searchString : "",	
+		};
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	onSearchChange = (e) => {
+		this.setState({
+			searchString : e.target.value,
+			robots: robots.filter((robot) => {return (robot.name.toLowerCase().includes(e.target.value.toLowerCase()))})
+		})
+	}
+
+	render(){
+		return(
+			<div className = 'tc'>
+				<h1> RoboFriends </h1>
+				<SearchBox onSearchChange = {this.onSearchChange}/>
+				<CardList robots = {this.state.robots} />
+			</div>
+		);
+	}
+};
 
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = () => {
+const SearchBox = ({onSearchChange}) => {
 	return (
 		<div className = 'pa2'>
 			<input 
 			className = 'pa3 ba b--green bg-lightest-blue'
 			type = 'search' 
-			placeholder = 'Search robots' />
+			placeholder = 'Search robots' 
+			onChange = {onSearchChange}/>
 		</div>
 	);
 }
