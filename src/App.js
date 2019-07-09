92106526a8ff761213179a77fbb03d61bbc717ef
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-09 18:50:38 -0400

    Fetching robot list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './CardList';
-import { robots } from './robots'
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,27 +7,39 @@ class App extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			robots : robots,
+			robots : [],
 			searchString : "",	
 		};
 		this.onSearchChange = this.onSearchChange.bind(this);
 	}
 
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then((response) => response.json())
+		.then(users => this.setState({ robots : users}));	
+	}
+
 	onSearchChange = (e) => {
 		this.setState({
-			searchString : e.target.value,
-			robots: robots.filter((robot) => {return (robot.name.toLowerCase().includes(e.target.value.toLowerCase()))})
+			searchString : e.target.value
 		})
 	}
 
 	render(){
-		return(
-			<div className = 'tc'>
-				<h1 className = 'f1'> RoboFriends </h1>
-				<SearchBox onSearchChange = {this.onSearchChange}/>
-				<CardList robots = {this.state.robots} />
-			</div>
-		);
+		const filteredRobots = this.state.robots.filter((robot) => {return (robot.name.toLowerCase().includes(this.state.searchString.toLowerCase()))});
+		if(this.state.robots.length === 0){
+			return <h1 className = 'tc f1'>Loading  robots</h1>;
+		}
+		else{
+			return(
+				<div className = 'tc'>
+					<h1 className = 'f1'> RoboFriends </h1>
+					<SearchBox onSearchChange = {this.onSearchChange}/>
+					<CardList robots = {filteredRobots} />
+				</div>
+			);
+		}
 	}
 };
 
