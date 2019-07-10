749b22c140afd5e24af4eaeb35aac99d94e3c32e
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-09 21:37:08 -0400

    Deploying web-app

---
+++ build/precache-manifest.e066b55e364fbeefd2c74a7a624853e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148823cb1217030bc0a7311042be53f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf861e2598fd1bdea65",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d40ff462ac11adbe0991",
+    "url": "/static/css/main.af0c3301.chunk.css"
+  },
+  {
+    "revision": "fcf861e2598fd1bdea65",
+    "url": "/static/js/2.7616bd35.chunk.js"
+  },
+  {
+    "revision": "d40ff462ac11adbe0991",
+    "url": "/static/js/main.77f50934.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
