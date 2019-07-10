87b4c7039841fbdcae63ded8df7c5493ab5c04e4
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-09 20:17:18 -0400

    Adding scroll element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css';
 
 class App extends React.Component {
@@ -36,7 +37,9 @@ class App extends React.Component {
 				<div className = 'tc'>
 					<h1 className = 'f1'> RoboFriends </h1>
 					<SearchBox onSearchChange = {this.onSearchChange}/>
-					<CardList robots = {filteredRobots} />
+					<Scroll>
+						<CardList robots = {filteredRobots} />
+					</Scroll>
 				</div>
 			);
 		}
